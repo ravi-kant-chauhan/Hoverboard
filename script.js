2d8ed86a9ece08dbe7c78a0a01ff16f279483428
Ravi <ravkchauhan1aip@gmail.com>
2023-03-31 23:22:45 +0530

    update

--- script.js
+++ script.js
@@ -5,4 +5,8 @@ const SQUARES = 500
 for(let i = 0; i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseout', () => removeColor(square))
+    container.appendChild(square)
 }
